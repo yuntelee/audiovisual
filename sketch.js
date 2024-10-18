4ca7ef4ef58f3823f7dfc445c7577913b9947a86
yunte lee <jyl2199@columbia.edu>
2024-10-17 20:39:38 -0400

    music now playable

--- sketch.js
+++ sketch.js
@@ -1,128 +1,189 @@
-// Define variables for canvas size
 let canvasWidth = screen.availWidth;
 let canvasHeight = screen.availHeight;
 
-let perspective = screen.availWidth/600;
-let factor = canvasWidth /50;
+let perspective = screen.availWidth / 600;
+let factor = canvasWidth / 50;
 let c1, c2, c3, c4, c5, c6, c7;
+
+let gtr, inst, beef; // Audio elements
+let gtrSrc, instSrc, beefSrc; // Media element sources
+let gtrContext, instContext, beefContext; // Audio contexts
+let audioLoaded = false; // Track whether audio has been loaded
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  c7 = new DraggableCircle(canvasWidth * 0, canvasHeight * 0.55, 20, 4*perspective);
-  c1 = new DraggableCircle(canvasWidth * 0.57, canvasHeight * 0.67, 20, 6*perspective);
-  c2 = new DraggableCircle(canvasWidth * 0.75, canvasHeight * 0.53, 20, 4*perspective);
-  c6 = new DraggableCircle(canvasWidth * 1.35, canvasHeight * 0.5, 20, 6*perspective);
-  c3 = new DraggableCircle(canvasWidth, canvasHeight * 0.47, 20, 3*perspective);
-  c4 = new DraggableCircle(canvasWidth * 0.33, canvasHeight * 0.53, 20, 3*perspective);
-  c5 = new DraggableCircle(canvasWidth * 0.25, canvasHeight * 0.73, 20, 1*perspective);
   
+  // Initialize draggable circles
+  c7 = new DraggableCircle(canvasWidth * 0, canvasHeight * 0.55, 20, 4 * perspective);
+  c1 = new DraggableCircle(canvasWidth * 0.57, canvasHeight * 0.67, 20, 6 * perspective);
+  c2 = new DraggableCircle(canvasWidth * 0.75, canvasHeight * 0.53, 20, 4 * perspective);
+  c6 = new DraggableCircle(canvasWidth * 1.35, canvasHeight * 0.5, 20, 6 * perspective);
+  c3 = new DraggableCircle(canvasWidth, canvasHeight * 0.47, 20, 3 * perspective);
+  c4 = new DraggableCircle(canvasWidth * 0.33, canvasHeight * 0.53, 20, 3 * perspective);
+  c5 = new DraggableCircle(canvasWidth * 0.25, canvasHeight * 0.73, 20, 1 * perspective);
 }
-
 
 function draw() {
   background(100);
   strokeWeight(0);
 
-  // Top quad (First layer)
+  // Draw quads
   fill(255, 249, 201);
-  quad(
-    0, 0,
-    canvasWidth, 0,
-    canvasWidth, canvasHeight,
-    0, canvasHeight
-  );
+  quad(0, 0, canvasWidth, 0, canvasWidth, canvasHeight, 0, canvasHeight);
 
-  // Second quad
   fill(255, 212, 151);
   quad(
-    0 - (Math.abs(mouseX*2 /factor)), canvasHeight * 0.13,
-    canvasWidth + (Math.abs(mouseX*2 /factor)), canvasHeight * 0.2,
+    0 - (Math.abs(mouseX * 2 / factor)), canvasHeight * 0.13,
+    canvasWidth + (Math.abs(mouseX * 2 / factor)), canvasHeight * 0.2,
     canvasWidth, canvasHeight,
     0, canvasHeight
   );
 
-  // Third quad
   fill(251, 165, 139);
   quad(
-    0 - (Math.abs(mouseX*3 /factor)), canvasHeight * 0.37,
-    canvasWidth + (Math.abs(mouseX*3 /factor)), canvasHeight * 0.33,
+    0 - (Math.abs(mouseX * 3 / factor)), canvasHeight * 0.37,
+    canvasWidth + (Math.abs(mouseX * 3 / factor)), canvasHeight * 0.33,
     canvasWidth, canvasHeight,
     0, canvasHeight
   );
 
-  // Bottom quad
   fill(251, 145, 143);
   quad(
-    0 - (Math.abs(mouseX*4 /factor)), canvasHeight * 0.57,
-    canvasWidth + (Math.abs(mouseX*4 /factor)), canvasHeight * 0.53,
+    0 - (Math.abs(mouseX * 4 / factor)), canvasHeight * 0.57,
+    canvasWidth + (Math.abs(mouseX * 4 / factor)), canvasHeight * 0.53,
     canvasWidth, canvasHeight,
     0, canvasHeight
   );
 
   drawTriangles();
+
+  rectMode(CENTER);
+  fill(100, 150, 255);
+  stroke(0);
+  rect(canvasWidth / 2, canvasHeight / 2, canvasWidth / 6, canvasHeight / 6, 20);
+
+  fill(0, 0, 0);
+  textAlign(CENTER, CENTER);
+  textSize(100);
+  text('play', canvasWidth / 2, canvasHeight / 2);
+
+  // Play audio if the button is pressed
+  if (audioLoaded && ball()) {
+    gtr.play();
+    inst.play();
+    beef.play();
+  }
+}
+
+function ball() {
+  if (mouseX < width / 2 + width / 12 &&
+      mouseX > width / 2 - width / 12 &&
+      mouseY < height / 2 + height / 12 &&
+      mouseY > height / 2 - height / 12
+  ) {
+    rectMode(CENTER);
+    fill(39, 3, 3);
+    stroke(0);
+    rect(canvasWidth / 2, canvasHeight / 2, canvasWidth / 6, canvasHeight / 6, 20);
+    
+    fill(0, 0, 0);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    text('play', canvasWidth / 2, canvasHeight / 2);
+
+    return mouseIsPressed; // Return true if the mouse is pressed
+  }
+  
+  return false; // Return false if conditions are not met
+}
+
+function mouseClicked() {
+  // Check if play button was clicked
+  if (mouseX > width / 2 - width / 12 &&
+      mouseX < width / 2 + width / 12 &&
+      mouseY > height / 2 - height / 12 &&
+      mouseY < height / 2 + height / 12
+  ) {
+    loadAudio(); // Load audio when the button is clicked
+  }
+}
+
+function loadAudio() {
+  // Only load audio once
+  if (!audioLoaded) {
+    gtrContext = new AudioContext();
+    instContext = new AudioContext();
+    beefContext = new AudioContext();
+
+    gtr = new Audio("gtr.mp3");
+    inst = new Audio("inst.mp3");
+    beef = new Audio("beef.mp3");
+
+    // Create media element sources
+    gtrSrc = gtrContext.createMediaElementSource(gtr);
+    instSrc = instContext.createMediaElementSource(inst);
+    beefSrc = beefContext.createMediaElementSource(beef);
+
+    // Connect sources to destination
+    gtrSrc.connect(gtrContext.destination);
+    instSrc.connect(instContext.destination);
+    beefSrc.connect(beefContext.destination);
+
+    // Set audio loaded to true
+    audioLoaded = true;
+    console.log("Audio loaded.");
+  }
 }
 
 function drawTriangles() {
-
-  
-
-
-  // Most back middle small triangle
+  // Draw triangles
   fill(73, 73, 75);
   triangle(
-    canvasWidth * 0.22 - (mouseX*c1.getMoveFactor() /factor), canvasHeight,
+    canvasWidth * 0.22 - (mouseX * c1.getMoveFactor() / factor), canvasHeight,
     c1.getX(), c1.y,
-    canvasWidth * 0.9 - (mouseX*c1.getMoveFactor() /factor), canvasHeight
-  );
-  
-        // t most back middle big triangle6
-  fill(73, 73, 75);
-  triangle(
-    canvasWidth * 0.8 - (mouseX*c6.getMoveFactor() /factor), canvasHeight,
-    c6.getX(), c6.y,
-    canvasWidth * 1.6 - (mouseX*c6.getMoveFactor() /factor), canvasHeight
-  );
-  
-    //most back left
-   fill(114, 118, 123);
-  triangle(
-    canvasWidth * -0.4 - (mouseX*c7.getMoveFactor() /factor), canvasHeight,
-    c7.getX(), c7.y,
-    canvasWidth * 0.4 - (mouseX*c7.getMoveFactor() /factor), canvasHeight
+    canvasWidth * 0.9 - (mouseX * c1.getMoveFactor() / factor), canvasHeight
   );
 
-  // Second back middle big triangle
+  fill(73, 73, 75);
+  triangle(
+    canvasWidth * 0.8 - (mouseX * c6.getMoveFactor() / factor), canvasHeight,
+    c6.getX(), c6.y,
+    canvasWidth * 1.6 - (mouseX * c6.getMoveFactor() / factor), canvasHeight
+  );
+
   fill(114, 118, 123);
   triangle(
-    canvasWidth * 0.5 - (mouseX*c2.getMoveFactor() /factor), canvasHeight,
+    canvasWidth * -0.4 - (mouseX * c7.getMoveFactor() / factor), canvasHeight,
+    c7.getX(), c7.y,
+    canvasWidth * 0.4 - (mouseX * c7.getMoveFactor() / factor), canvasHeight
+  );
+
+  fill(114, 118, 123);
+  triangle(
+    canvasWidth * 0.5 - (mouseX * c2.getMoveFactor() / factor), canvasHeight,
     c2.getX(), c2.y,
-    canvasWidth - (mouseX*c2.getMoveFactor() /factor), canvasHeight
+    canvasWidth - (mouseX * c2.getMoveFactor() / factor), canvasHeight
   );
-  
 
-
-  // Big right triangle
   fill(130, 134, 138);
   triangle(
-    canvasWidth * 0.58 - (mouseX*c3.getMoveFactor() /factor), canvasHeight,
+    canvasWidth * 0.58 - (mouseX * c3.getMoveFactor() / factor), canvasHeight,
     c3.getX(), c3.y,
-    canvasWidth * 1.25 - (mouseX*c3.getMoveFactor() /factor), canvasHeight
+    canvasWidth * 1.25 - (mouseX * c3.getMoveFactor() / factor), canvasHeight
   );
 
-  // Front big left triangle
   fill(130, 134, 138);
   triangle(
-    canvasWidth * -0.17 - (mouseX*c4.getMoveFactor() /factor), canvasHeight,
+    canvasWidth * -0.17 - (mouseX * c4.getMoveFactor() / factor), canvasHeight,
     c4.getX(), c4.y,
-    canvasWidth * 0.67 - (mouseX*c4.getMoveFactor() /factor), canvasHeight
+    canvasWidth * 0.67 - (mouseX * c4.getMoveFactor() / factor), canvasHeight
   );
 
-  // Last triangle (m5)
   fill(166, 169, 172);
   triangle(
-    canvasWidth * -0.08 - (mouseX*c5.getMoveFactor() /factor), canvasHeight,
+    canvasWidth * -0.08 - (mouseX * c5.getMoveFactor() / factor), canvasHeight,
     c5.getX(), c5.y,
-    canvasWidth * 0.55 - (mouseX*c5.getMoveFactor() /factor), canvasHeight
+    canvasWidth * 0.55 - (mouseX * c5.getMoveFactor() / factor), canvasHeight
   );
 
   // Display and update all circles
@@ -173,11 +234,7 @@ class DraggableCircle {
     this.hovered = d < this.r;
 
     if (this.dragging) {
-      
-      
-      this. y = constrain(lerp(this.y, py, 0.1), this.r*4, canvasHeight - this.r*2);
-      
-      
+      this.y = constrain(lerp(this.y, py, 0.1), this.r * 4, canvasHeight - this.r * 2);
     }
   }
 
